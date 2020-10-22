d56d137d8813043e594f657bb4459d32a1a1a790
HenryDewa <henry@finasmart.com>
2020-10-22 23:21:45 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import {Component} from '@angular/core';
-import {Subject} from 'rxjs/Subject';
-import {Observable} from 'rxjs/Observable';
-import {WebcamImage} from 'ngx-webcam';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Subject } from "rxjs/Subject";
+import { Observable } from "rxjs/Observable";
+import { WebcamImage } from "ngx-webcam";
+import * as uuid from "uuid";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  myId = uuid.v4();
   // latest snapshot
   public webcamImage: WebcamImage = null;
 
   handleImage(webcamImage: WebcamImage) {
     this.webcamImage = webcamImage;
+  }
+
+  public ngOnInit(): void {
+    console.log("UUID => " + this.myId);
   }
 }
